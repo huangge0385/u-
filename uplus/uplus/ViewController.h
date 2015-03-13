f66d43f22eac92c000378f221d6edcb761f194ab
huangge0385 <huangge@lawn-143-215-62-177.lawn.gatech.edu>
2015-03-13 13:21:02 -0400

    create project

---
+++ uplus/uplus/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  uplus
+//
+//  Created by 黄舸 on 15-3-13.
+//  Copyright (c) 2015年 CS6365. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ uplus/uplus/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  uplus
+//
+//  Created by 黄舸 on 15-3-13.
+//  Copyright (c) 2015年 CS6365. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
